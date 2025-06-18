22e2377977e8c20bbc91159705c57267437f31e5
SwadeepSinghaRoy <swadeep5681@gmail.com>
2025-06-18 13:28:33 +0530

    Working on Admin's Check Users power(main section)

--- CHECK-USERS/index.php
+++ CHECK-USERS/index.php
@@ -23,7 +23,26 @@
                 <input type="text" placeholder=" Search..." class="search-bar">
               </div>
             </header>
-            <main></main>
+            <main>
+              <div class="show-users">
+                <div class="table-section">
+                  <table class="user-info-table">
+                    <thead>
+                      <tr>
+                        <th>Id</th>
+                        <th>User Type Id</th>
+                        <th>Name</th>
+                        <th>User Email</th>
+                        <th>User Password</th>
+                        <th>Created At</th>
+                        <th>Updated At</th>
+                        <th>Delete</th>
+                      </tr>
+                    </thead>
+                  </table>
+                </div>
+              </div>
+            </main>
         </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
